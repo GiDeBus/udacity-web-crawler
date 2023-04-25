91800cf49dbd50dc47d1fa48aa29b1a9cc1f31d6
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-25 14:03:48 -0400

    implement writer

--- src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -37,6 +37,15 @@ public final class WebCrawlerMain {
     CrawlResult result = crawler.crawl(config.getStartPages());
     CrawlResultWriter resultWriter = new CrawlResultWriter(result);
     // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
+
+    if (!config.getResultPath().isEmpty()) {
+      Path outputPath = Path.of(config.getResultPath());
+      resultWriter.write(outputPath);
+    } else {
+      Writer writer = new BufferedWriter(new OutputStreamWriter(System.out));
+      resultWriter.write(writer);
+    }
+
     // TODO: Write the profile data to a text file (or System.out if the file name is empty)
   }
 
