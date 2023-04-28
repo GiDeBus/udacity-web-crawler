85aa7dc6ff6b3b6bd597c613007ac9cb59b763c5
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-04-28 13:40:52 -0400

    new compute method override

--- src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
+++ src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
@@ -1,98 +1,84 @@
 package com.udacity.webcrawler;
 
 import com.udacity.webcrawler.json.CrawlResult;
-import com.udacity.webcrawler.parser.PageParser;
-import com.udacity.webcrawler.parser.PageParserFactory;
 
 import javax.inject.Inject;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 import java.util.regex.Pattern;
-import java.util.ArrayList;
 
-import static java.util.concurrent.ForkJoinTask.invokeAll;
+import com.udacity.webcrawler.parser.PageParser;
+import com.udacity.webcrawler.parser.PageParserFactory;
 
 /**
  * A concrete implementation of {@link WebCrawler} that runs multiple threads on a
  * {@link ForkJoinPool} to fetch and process multiple web pages in parallel.
  */
 final class ParallelWebCrawler implements WebCrawler {
-
   private final Clock clock;
-  private final PageParserFactory parserFactory;
   private final Duration timeout;
   private final int popularWordCount;
+  private final ForkJoinPool pool;
+  private final PageParserFactory parserFactory;
   private final int maxDepth;
   private final List<Pattern> ignoredUrls;
-  private final ForkJoinPool pool;
 
   @Inject
   ParallelWebCrawler(
-      Clock clock,
-      PageParserFactory parserFactory,
-      @Timeout Duration timeout,
-      @PopularWordCount int popularWordCount,
-      @MaxDepth int maxDepth,
-      @IgnoredUrls List<Pattern> ignoredUrls,
-      @TargetParallelism int threadCount
+          Clock clock,
+          @Timeout Duration timeout,
+          @PopularWordCount int popularWordCount,
+          @TargetParallelism int threadCount,
+          PageParserFactory parserFactory,
+          @MaxDepth int maxDepth,
+          @IgnoredUrls List<Pattern> ignoredUrls
   ) {
     this.clock = clock;
-    this.parserFactory = parserFactory;
     this.timeout = timeout;
     this.popularWordCount = popularWordCount;
+    this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
+    this.parserFactory = parserFactory;
     this.maxDepth = maxDepth;
     this.ignoredUrls = ignoredUrls;
-    this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
   }
 
   @Override
   public CrawlResult crawl(List<String> startingUrls) {
-    Instant deadline = clock.instant().plus(timeout);
-
-    Map<String, Integer> counts = new ConcurrentHashMap<>();
-    Set<String> visitedUrls = new ConcurrentSkipListSet<>();
-
-    for (String url : startingUrls) {
-
-      pool.invoke(new CustomConcurrentTask(url, deadline, maxDepth, counts, visitedUrls));
-    }
-
-    if (counts.isEmpty()) {
-      return new CrawlResult.Builder()
-              .setWordCounts(counts)
-              .setUrlsVisited(visitedUrls.size())
-              .build();
-    }
-    return new CrawlResult.Builder()
-            .setWordCounts(WordCounts.sort(counts, popularWordCount))
-            .setUrlsVisited(visitedUrls.size())
-            .build();
+    return new CrawlResult.Builder().build();
   }
 
   @Override
   public int getMaxParallelism() {
     return Runtime.getRuntime().availableProcessors();
   }
-  private class CustomConcurrentTask extends RecursiveTask<Map<String, Integer>> {
-    String url;
-    Instant deadline;
-    int maxDepth;
-    Map<String, Integer> counts;
-    Set<String> visitedUrls;
 
-    private CustomConcurrentTask(
-            String url,
-            Instant deadline,
+  // TODO: Complete method
+  public final class CustomCrawlTask extends RecursiveTask {
+    private final int maxDepth;
+    private final Instant deadline;
+    private final String url;
+    private final ConcurrentHashMap<String, Integer> counts;
+    private final ConcurrentSkipListSet<String> visitedUrls;
+
+    public CustomCrawlTask(
             int maxDepth,
-            Map<String, Integer> counts,
-            Set<String> visitedUrls
+            Instant deadline,
+            String url,
+            ConcurrentHashMap<String, Integer> counts,
+            ConcurrentSkipListSet<String> visitedUrls
     ) {
-      this.url = url;
-      this.deadline = deadline;
       this.maxDepth = maxDepth;
+      this.deadline = deadline;
+      this.url = url;
       this.counts = counts;
       this.visitedUrls = visitedUrls;
     }
@@ -112,23 +98,16 @@ final class ParallelWebCrawler implements WebCrawler {
       }
       visitedUrls.add(url);
       PageParser.Result result = parserFactory.get(url).parse();
-      Map<String, Integer> localCounts = new HashMap<>();
-      for (Map.Entry<String, Integer> e : result.getWordCounts().entrySet()) {
-        localCounts.merge(e.getKey(), e.getValue(), Integer::sum);
-      }
-      List<CustomConcurrentTask> subTasks = new ArrayList<>();
-      for (String link : result.getLinks()) {
-        subTasks.add(new CustomConcurrentTask(link, deadline, maxDepth - 1, counts, visitedUrls));
-      }
-      invokeAll(subTasks);
-      for (CustomConcurrentTask subTask : subTasks) {
-        Map<String, Integer> subCounts = subTask.join();
-        for (Map.Entry<String, Integer> e : subCounts.entrySet()) {
-          localCounts.merge(e.getKey(), e.getValue(), Integer::sum);
-        }
-      }
-      return localCounts;
+      result.getWordCounts().entrySet()
+              .stream()
+              .forEach(s -> counts.compute(s.getKey(), (key, value) -> (value != null) ? s.getValue() + value : s.getValue()));
+
+      List<CustomCrawlTask> customCrawlTask = result.getLinks().stream()
+              .map(link -> new CustomCrawlTask(maxDepth - 1, deadline, link, counts, visitedUrls))
+              .collect(Collectors.toList());
+      invokeAll(customCrawlTask);
+
+      return counts;
     }
   }
-
 }
